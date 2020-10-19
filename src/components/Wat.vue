9d2be43ea0e66aa211ea9e70c3e94e80a6a114f4
dylan <dylankphan@gmail.com>
2020-10-18 21:08:20 -0700

    organs!

--- src/components/Wat.vue
+++ src/components/Wat.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class='Wat'>
+  <div class="Wat">
     <WatNode />
   </div>
 </template>
@@ -11,12 +11,19 @@ import Expressor from "./Expressor";
 import TestRunner from "./tests/TestRunner.vue";
 import Component from "vue-class-component";
 import { Prop } from "vue-property-decorator";
+import Vue from "vue";
 
-
-@Component({components: {
-  WatNode
-}})
-export default class Wat {
+@Component({
+  components: {
+    WatNode,
+  },
+})
+export default class Wat extends Vue {
+  mounted() {
+    document.addEventListener("mousedown", (event) => {
+      console.log(`${event.x} ${event.y}`);
+    });
+  }
 }
 </script>
 <style scoped>
--- src/components/WatNode.vue
+++ src/components/WatNode.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="children">
     <div v-for="(value, key) in subroot" :key="key" class="child">
-      <div class="card">{{ key }}</div>
-      <WatNode :subroot="value" :name="key" />
+      <div :id="cardIdFromName(key)" class="card">{{ key }}</div>
+      <WatLine
+        :cardId="cardIdFromName(key)"
+        :node="nodeFromName(key)"
+      ></WatLine>
+      <WatNode
+        ref="node"
+        :idid="nodeIdFromName(key)"
+        :id="nodeIdFromName(key)"
+        :subroot="value"
+      />
     </div>
   </div>
 </template>
@@ -13,8 +22,17 @@ import Expressor from "./Expressor";
 import TestRunner from "./tests/TestRunner.vue";
 import Component from "vue-class-component";
 import { Prop } from "vue-property-decorator";
+import WatLine from "./WatLine.vue";
+import Vue from "vue";
 
-const root = {
+let root = {
+  "the void": {
+    earth: null,
+    moon: null,
+  },
+};
+
+root = {
   "the void": {
     earth: {
       california: {
@@ -39,10 +57,45 @@ const root = {
   },
 };
 
-@Component()
-export default class WatNode {
+@Component({ components: { WatLine } })
+export default class WatNode extends Vue {
+  @Prop() id;
   @Prop({ default: () => root }) subroot;
-  @Prop({ default: "the void" }) name;
+  updateit = 0;
+
+  mounted() {
+    this.updateit++;
+  }
+
+  cardIds() {
+    this.updateit;
+    const ret = [];
+    if (this.subroot === null) return ret;
+    for (const [key, value] of Object.entries(this.subroot)) {
+      ret.push(this.cardIdFromName(key));
+    }
+    return ret;
+  }
+
+  cardIdFromName(name) {
+    this.updateit;
+    return "card " + name;
+  }
+
+  nodeFromName(name) {
+    this.updateit;
+    if (!this.$refs["node"]) return;
+    const node = this.$refs["node"].find((t) => {
+      return t.id === this.nodeIdFromName(name);
+    });
+    console.assert(node);
+    return node;
+  }
+
+  nodeIdFromName(name) {
+    this.updateit;
+    return "node " + name;
+  }
 }
 </script>
 <style scoped>
@@ -51,19 +104,30 @@ export default class WatNode {
   background: #fffcf9;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);
   padding: 10px;
-  max-width: 100%;
-  overflow: hidden;
+  /* max-width: 100%; */
+  /* overflow: hidden; */
 }
 .children {
-  display: grid;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  /* display: grid;
   grid-auto-flow: column;
   grid-auto-columns: 1fr;
   grid-template-rows: 100%;
-  overflow: hidden;
+  overflow: hidden; */
 }
 .child {
-  position: relative;
+  /* position: relative;
   grid-template-rows: min-content min-content;
-  display: grid;
+  display: grid; */
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  flex-direction: column;
+}
+.line {
+  border: 1px solid rgba(180, 170, 135, 0.6);
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.2);
 }
 </style>
