63bc5c91afa02ad62cf46f8657b64a09b4d10e5f
Dylan Phan <dylan.phan@tixr.com>
2024-05-05 16:25:01 -0700

    checkpoint at 5/5/24 4:25P

--- cpp/src/EvalContext.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Dylan on 7/11/2021.
-//
-
-#include "EvalContext.h"
-
-void EvalContext::setValue(const IntrinsicAttribute *attribute, float value) {
-    _valueByIntrinsicAttribute[attribute] = value;
-}
--- js/node_modules/@wasm-tool/emscripten/example/src/index.js
+++
@@ -1,3 +0,0 @@
-import("./fib.c").then(({_fib}) => {
-  document.body.innerHTML = "fib(10) = " + _fib(10);
-});
