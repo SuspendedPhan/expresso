30eadf48d66791944ade20d5f265060228b3a22c
Dylan Phan <dylan.phan@tixr.com>
2024-06-05 10:34:25 -0700

    checkpoint at 6/5/24 10:34P

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,30 @@
+<script lang="ts">
+  import "./app.css";
+  import { BehaviorSubject, combineLatest, map } from "rxjs";
+  import GoModuleLoader from "./GoModuleLoader";
+  import { NumberExpr } from "./Domain";
 
+  const goModule$ = GoModuleLoader.get$();
+  const expr = new BehaviorSubject(new NumberExpr(0));
+
+  const result = combineLatest([goModule$, expr]).pipe(
+    map(([goModule, expr]) => {
+      const evaluator = goModule.createEvaluator(expr);
+      return evaluator.eval();
+    })
+  );
+
+  function handleInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = parseFloat(target.value);
+    expr.next(new NumberExpr(value));
+  }
+</script>
 
 <main>
   <div>Hello World</div>
-  <input type="number" value="{$expr.getValue()}" on:input="{handleInput}" />
+  <input type="number" value={$expr.getValue()} on:input={handleInput} />
   <div>{$result}</div>
 </main>
-<script>import "./app.css";
-import { BehaviorSubject, combineLatest, map } from "rxjs";
-import GoModuleLoader from "./GoModuleLoader";
-import { NumberExpr } from "./Domain";
 
-const goModule$ = GoModuleLoader.get$();
-const expr = new BehaviorSubject(new NumberExpr(0));
-
-const result = combineLatest([goModule$, expr]).pipe(
-  map(([goModule, expr]) => {
-    const evaluator = goModule.createEvaluator(expr);
-    return evaluator.eval();
-  })
-);
-
-function handleInput(event: Event) {
-  const target = event.target as HTMLInputElement;
-  const value = parseFloat(target.value);
-  expr.next(new NumberExpr(value));
-}</script><style></style>
+<style></style>
