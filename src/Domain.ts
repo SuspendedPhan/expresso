f561c1bcb822dde3e24b8615b32ef57569c1ea3a
Dylan Phan <dylan.phan@tixr.com>
2024-06-05 09:51:50 -0700

    checkpoint at 6/5/24 9:51P

---
+++ src/Domain.ts
@@ -0,0 +1,62 @@
+import { BehaviorSubject, Observable } from 'rxjs'
+
+export class Component {
+  public id: string = 'component' + Math.random().toString(36).substring(7)
+  private attributes = new BehaviorSubject<Array<Attribute>>([])
+
+  public addAttribute() {
+    this.attributes.value.push(new Attribute())
+    this.attributes.next(this.attributes.value)
+  }
+
+  public getAttributes(): Observable<Array<Attribute>> {
+    return this.attributes
+  }
+}
+
+export class Attribute {
+  public id: string = 'attribute' + Math.random().toString(36).substring(7)
+  private expr = new BehaviorSubject(new NumberExpr(0))
+
+  public getExpr(): Observable<Expr> {
+    return this.expr
+  }
+}
+
+export interface Expr {}
+
+export class NumberExpr implements Expr {
+  public id: string = 'numberExpr' + Math.random().toString(36).substring(7)
+  private value: number
+
+  constructor(value: number) {
+    this.value = value
+  }
+
+  public getText(): string {
+    return this.value.toString()
+  }
+
+  getValue(): number {
+    return this.value
+  }
+}
+
+export class PrimitiveFunctionCallExpr {
+  public id: string = 'primitiveFunctionCallExpr' + Math.random().toString(36).substring(7)
+  private args = new BehaviorSubject<Array<Expr>>([])
+
+  constructor(private primitiveFunctionId: string) {}
+
+  public getPrimitiveFunctionId(): string {
+    return this.primitiveFunctionId
+  }
+
+  public getText(): string {
+    return this.primitiveFunctionId
+  }
+
+  public getArgs(): Observable<Array<Expr>> {
+    return this.args
+  }
+}
---
+++ src/GoModuleLoader.ts
@@ -0,0 +1,34 @@
+import { BehaviorSubject, Observable, Subject } from 'rxjs'
+import type { GoModule } from './GoModule'
+
+export default class GoModuleLoader {
+  private static goModuleSubject = new BehaviorSubject<GoModule | null>(null)
+
+  public static get$(): Observable<GoModule> {
+    return new Observable((observer) => {
+      if (this.goModuleSubject.value === null) {
+        // Subscribe to the subject to get notified when the library is loaded
+        this.instantiate()
+        this.goModuleSubject.subscribe({
+          complete: () => {
+            observer.next(this.goModuleSubject.value)
+          }
+        })
+      } else {
+        // Library is already loaded, emit the reference immediately
+        observer.next(this.goModuleSubject.value)
+      }
+    })
+  }
+
+  private static instantiate() {
+    const go = new Go()
+    WebAssembly.instantiateStreaming(fetch('mymodule.wasm'), go.importObject).then((result) => {
+      go.run(result.instance)
+      const goModule = window.GoModule
+      goModule.hello()
+      this.goModuleSubject.next(goModule)
+      this.goModuleSubject.complete()
+    })
+  }
+}
