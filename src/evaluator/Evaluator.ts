1305301a748c859cbd3e940c12d26d5ccb98754e
Dylan Phan <dylan.phan@tixr.com>
2024-07-08 21:24:53 -0700

    Need to implement eval and test it

--- src/evaluator/Evaluator.ts
+++ src/evaluator/Evaluator.ts
@@ -1,15 +1,31 @@
-import { Observable } from "rxjs";
+import { combineLatest, Observable, of } from "rxjs";
 import CircleComponent from "../domain/Component";
+import { Attribute } from "../Domain";
 
 export interface EvaluatedCircleClone {
-    x: number;
-    y: number;
-    radius: number;
+  x: number;
+  y: number;
+  radius: number;
 }
 
 export default class Evaluator {
-    public constructor(private circle$: Observable<CircleComponent>) {}
-    public evaluate$(): Observable<EvaluatedCircleClone[]> {
-        throw new Error("Not implemented");
-    }
+  public constructor(private circle$: Observable<CircleComponent>) {}
+  public evaluate$(): Observable<EvaluatedCircleClone[]> {
+    return new Observable<EvaluatedCircleClone[]>((subscriber) => {
+      this.circle$.subscribe((circleComponent) => {
+        combineLatest([
+          this.evalAttribute$(circleComponent.getX$()),
+          this.evalAttribute$(circleComponent.getY$()),
+          this.evalAttribute$(circleComponent.getRadius$()),
+        ]).subscribe(([x, y, radius]) => {
+          subscriber.next([{ x, y, radius }]);
+        });
+      });
+    });
+  }
+
+  private evalAttribute$(attribute$: Observable<Attribute>): Observable<number> {
+    // TODO: Implement this
+    return of(0);
+  }
 }
--- src/evaluator/EvaluatorTest.ts
+++ src/evaluator/EvaluatorTest.ts
@@ -3,6 +3,7 @@ import Evaluator from "./Evaluator";
 import CircleComponent from "../domain/Component";
 import Renderer from "../renderer/Renderer";
 import ScenePool from "../renderer/ScenePool";
+import { Attribute, NumberExpr } from "../Domain";
 
 export default class EvaluatorTest {
     public test() {
@@ -15,5 +16,16 @@ export default class EvaluatorTest {
             circles.push(circle);
             return circle;
         }));
+
+        const x$ = new Subject<Attribute>();
+        const y$ = new Subject<Attribute>();
+        const radius$ = new Subject<Attribute>();
+        const circleComponent = new CircleComponent(x$, y$, radius$);
+        circle$.next(circleComponent);
+        x$.next(new Attribute(new NumberExpr(0)));
+        y$.next(new Attribute(new NumberExpr(0)));
+        radius$.next(new Attribute(new NumberExpr(1)));
+
+        // TODO: verify that the circles are correctly positioned
     }
 }
