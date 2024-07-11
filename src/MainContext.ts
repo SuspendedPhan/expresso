fd892f3c81dfdc1c58379175c971b9207b20b81b
Dylan Phan <dylan.phan@tixr.com>
2024-07-10 18:04:39 -0700

    gobridge

--- src/MainContext.ts
+++ src/MainContext.ts
@@ -1,6 +1,7 @@
 import type GoModule from "./utils/GoModule";
 import ExprFactory from "./ExprFactory";
 import Selection from "./utils/Selection";
+import GoBridge from "./GoBridge";
 
 export default class MainContext {
   public constructor(
@@ -8,25 +9,6 @@ export default class MainContext {
     public readonly exprFactory: ExprFactory,
     public readonly selection: Selection,
   ) {
-    exprFactory.onNumberExprAdded$.subscribe((numberExpr) => {
-      goModule.addNumberExpr(numberExpr.id);
-      goModule.setNumberExprValue(numberExpr.id, numberExpr.value);
-    });
-
-    exprFactory.onCallExprAdded$.subscribe((callExpr) => {
-      goModule.addCallExpr(callExpr.id);
-
-      if (callExpr.args.length !== 2) {
-        throw new Error("CallExpr must have 2 args");
-      }
-
-      callExpr.args[0]?.subscribe((arg0) => {
-        goModule.setCallExprArg0(callExpr.id, arg0.id);
-      });
-
-      callExpr.args[1]?.subscribe((arg1) => {
-        goModule.setCallExprArg1(callExpr.id, arg1.id);
-      });
-    });
+    new GoBridge(goModule, exprFactory);
   }
 }
