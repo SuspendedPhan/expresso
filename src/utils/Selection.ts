b548e1946eefac7bfee6628967b03517559a330b
Dylan Phan <dylan.phan@tixr.com>
2024-07-10 17:10:40 -0700

    new sel

--- src/utils/Selection.ts
+++ src/utils/Selection.ts
@@ -3,19 +3,10 @@ import {
   Observable,
   combineLatest,
   map,
-  of,
   share,
-  switchAll,
-  switchMap,
+  take,
 } from "rxjs";
-// import { ReadonlyAttribute, ReadonlyCallExpr, ReadonlyExpr, ReadonlyNumberExpr } from "../Domain";
 import Logger from "./Logger";
-import { type ReadonlyAttribute } from "../Domain";
-import type {
-  ReadonlyCallExpr,
-  ReadonlyExpr,
-  ReadonlyNumberExpr,
-} from "../domain/Expr";
 import { Attribute, Expr } from "../ExprFactory";
 
 export type Selectable = Attribute | Expr | null;
@@ -24,96 +15,66 @@ const logger = Logger.file("Selection.ts");
 logger.allow();
 
 export default class Selection {
-  private selectedObject$ = new BehaviorSubject<Observable<Selectable>>(
-    of(null)
-  );
+  public readonly selectedObject$ = new BehaviorSubject<Selectable>(null);
+  private readonly downFn$: Observable<() => void>;
+  private readonly upFn$: Observable<() => void>;
 
-  public constructor(private root: Observable<Attribute>) {}
+  public constructor(private root: Observable<Attribute>) {
+    this.downFn$ = combineLatest([this.selectedObject$, this.root]).pipe(
+      map(([selectedObject, root]) => {
+        if (selectedObject === null) {
+          return () => this.selectedObject$.next(root);
+        }
 
-  public select(object$: Observable<Selectable>) {
-    if (object$ === null) {
-      this.selectedObject$.next(of(null));
-      return;
-    }
-    this.selectedObject$.next(object$);
+        if (selectedObject.type === "Attribute") {
+          return () => {
+            selectedObject.expr$.pipe(take(1)).subscribe(this.selectedObject$);
+          };
+        }
+
+        if (selectedObject.type === "CallExpr") {
+          return () => {
+            if (selectedObject.args[0] === undefined) {
+              throw new Error("CallExpr must have at least 1 args");
+            }
+            selectedObject.args[0]
+              .pipe(take(1))
+              .subscribe(this.selectedObject$);
+          };
+        }
+
+        if (selectedObject.type === "NumberExpr") {
+            return () => {};
+        }
+
+        throw new Error("Unknown type");
+      }),
+      share()
+    );
+
+    this.upFn$ = combineLatest([this.selectedObject$, this.root]).pipe(
+      map(([selectedObject, root]) => {
+        if (selectedObject === null) {
+            return () => this.selectedObject$.next(root);
+        }
+
+        if (selectedObject.type === "Attribute") {
+          return () => this.selectedObject$.next(null);
+        }
+
+        selectedObject.parent$.pipe(take(1)).subscribe(this.selectedObject$);
+
+        return () => {};
+      }),
+      share()
+    );
   }
 
   public down() {
-    logger.log("down");
-    const getNextObject$ = this.getChild$.bind(this);
-    this.handleNavigation(getNextObject$);
+    this.downFn$.pipe(take(1)).subscribe((fn) => fn());
   }
 
   public up() {
-    logger.log("up");
-    const getNextObject$ = this.getParent$.bind(this);
-    this.handleNavigation(getNextObject$);
-  }
-
-  private handleNavigation(
-    getNextObject$: (currentObject: Selectable) => Observable<Selectable>
-  ) {
-    const object$ = this.selectedObject$.value;
-    logger.log("selectedObject", object$);
-
-    const child$ = object$.pipe(
-      switchMap((object) => {
-        const child$ = getNextObject$(object);
-        return child$;
-      }),
-      switchMap((child) => {
-        if (child === null) {
-          return object$;
-        }
-        return of(child);
-      })
-    );
-    this.select(child$);
-  }
-
-  private getChild$(object: Selectable): Observable<Selectable> {
-    if (object === null) {
-      return this.root;
-    } else if (object.type === "Attribute") {
-      return object.expr$;
-    } else {
-      return this.getChildForExpr$(object);
-    }
-  }
-
-  private getParent$(object: Selectable): Observable<Selectable> {
-    return this.root.pipe(
-      switchMap((root) => {
-        if (object === null) {
-          return of(null);
-        } else if (object === root) {
-          return of(null);
-        } else if (object.type === "Attribute") {
-          throw new Error("Not implemented");
-        } else if (object) {
-          return object.parent$;
-        } else {
-          logger.log("object", object);
-          throw new Error("Unknown object type");
-        }
-      })
-    );
-  }
-
-  private getChildForExpr$(expr: Expr): Observable<Selectable> {
-    if (expr.type === "NumberExpr") {
-      return of(null);
-    } else if (expr.type === "CallExpr") {
-      if (expr.args[0] === undefined) {
-        throw new Error("No args");
-      }
-      return expr.args[0];
-    } else {
-      throw new Error("Unknown expr type");
-    }
-  }
-
-  public getSelectedObject$(): Observable<Selectable> {
-    return this.selectedObject$.pipe(switchAll(), share());
+    this.upFn$.pipe(take(1)).subscribe((fn) => fn());
   }
 }
