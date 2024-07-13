5ba51f327d57eb9aafb8a3e36915e5206e788d90
Dylan Phan <dylan.phan@tixr.com>
2024-07-12 21:30:13 -0700

    coool

--- src/utils/YY.ts
+++ src/utils/YY.ts
@@ -3,23 +3,14 @@ import {
   map,
   MonoTypeOperatorFunction,
   of,
-  OperatorFunction,
-  pipe,
-  switchAll,
   switchMap,
   tap,
 } from "rxjs";
-import {
-  AstFunctionCall,
-  loggedMethod,
-  LoggerDecorator,
-} from "../logger/LoggerDecorator";
+import { loggedMethod, LoggerDecorator } from "../logger/LoggerDecorator";
 import Logger from "../logger/Logger";
 
 export default function YY() {
   QQ.ta$().subscribe(() => {});
-
-  // QQ.tt$.next(2);
 }
 
 class QQ {
@@ -32,16 +23,15 @@ class QQ {
 
     return this.tt$.pipe(
       map((v) => {
-        logger.log("map", {
-          v,
-        });
+        logger.log("map", v);
         return v + 1;
       }),
       switchMap((v) => {
-        logger.log("switchMap", {
-          v,
-        });
+        logger.log("switchMap", v);
         return this.tb$(v);
+      }),
+      tap((v) => {
+        logger.log("tap", v);
       })
     );
   }
@@ -52,33 +42,12 @@ class QQ {
     return of(v).pipe(
       map((v) => {
         return v + 2;
-      }),
+      })
     );
   }
 }
 
 class LL {
-  static tapLog<T>(
-    name: string,
-    callback: (...args: any[]) => [string, any][]
-  ): MonoTypeOperatorFunction<T> {
-    const currentCall = LoggerDecorator.currentCall$.value;
-    if (!currentCall) {
-      throw new Error("No current function call");
-    }
-
-    return tap((...args) => {
-      const astCall = currentCall.astCall;
-      if (!astCall.currentlyLogging) {
-        return;
-      }
-
-      const tapLogArgs = callback(...args);
-      const argString = tapLogArgs.map((arg) => arg.join(": ")).join(", ");
-      console.log(`${astCall.id}.${name}(${argString})`);
-    });
-  }
-
   static logger() {
     const currentCall = LoggerDecorator.currentCall$.value;
     if (!currentCall) {
@@ -86,14 +55,14 @@ class LL {
     }
 
     return {
-      log(name: string, args: any) {
+      log(name: string, ...args: any) {
         const astCall = currentCall.astCall;
         if (!astCall.currentlyLogging) {
           return;
         }
 
         // concat args
-        const argString = Object.entries(args).map(([key, value]) => `${key}: ${value}`).join(", ");
+        const argString = args.map((arg: any) => arg.toString()).join(", ");
         console.log(`${astCall.id}.${name}(${argString})`);
       },
     };
