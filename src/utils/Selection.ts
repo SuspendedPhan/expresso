cd98508390d1e640b14fb2e5afddecbadf3c0539
Dylan Phan <dylan.phan@tixr.com>
2024-07-15 14:09:13 -0700

    2

--- src/utils/Selection.ts
+++ src/utils/Selection.ts
@@ -1,76 +1,120 @@
-import { BehaviorSubject, Observable, of, take } from "rxjs";
+import {
+  BehaviorSubject,
+  combineLatest,
+  first,
+  Observable,
+  of,
+  Subject,
+  switchAll,
+  take,
+} from "rxjs";
 import Logger from "./Logger";
-import { Attribute, Expr } from "../ExprFactory";
+import {
+  ExObject,
+  ExObjectType,
+  Expr,
+  ExprType,
+} from "../ExprFactory";
 import ExprManager from "../ExprManager";
+import { assertUnreachable } from "./Utils";
 
-export type Selectable = Attribute | Expr | null;
+export type Selectable = ExObject | null;
 
 const logger = Logger.file("Selection.ts");
 logger.allow();
 
 export default class Selection {
-  private readonly selectedObject$$ = new BehaviorSubject<Observable<Selectable>>(of());
+  private readonly selectedObject$$ = new BehaviorSubject<
+    Observable<Selectable>
+  >(of(null));
   public readonly root$ = new BehaviorSubject<Selectable>(null);
-  public constructor(private exprManager: ExprManager) {}
+  public readonly down$ = new Subject<void>();
+  public readonly up$ = new Subject<void>();
+  public constructor(private exprManager: ExprManager) {
+    combineLatest([this.down$, this.selectedObject$$]).subscribe(
+      ([_, selectedObject$]) => {
+        selectedObject$.pipe(first()).subscribe((selectedObject) => {
+          this.down(selectedObject);
+        });
+      }
+    );
+
+    combineLatest([this.up$, this.selectedObject$$]).subscribe(
+      ([_, selectedObject$]) => {
+        selectedObject$.pipe(first()).subscribe((selectedObject) => {
+          this.up(selectedObject);
+        });
+      }
+    );
+  }
+
+  public getSelectedObject$(): Observable<Selectable> {
+    return this.selectedObject$$.pipe(switchAll());
+  }
 
   public select(object: Selectable) {
+    if (object === null) {
+      this.selectedObject$$.next(of(null));
+      return;
+    }
     const object$ = this.exprManager.getObject$(object);
     this.selectedObject$$.next(object$);
   }
 
-  public down() {
-    const selectedObject = this.selectedObject$.value;
-    const root = this.root$.value;
-
+  public down(selectedObject: Selectable) {
     if (selectedObject === null) {
-      this.selectedObject$.next(root);
+      this.selectedObject$$.next(this.root$);
       return;
     }
 
-    if (selectedObject.type === "Attribute") {
-      selectedObject.expr$.pipe(take(1)).subscribe((expr) => {
-        this.selectedObject$.next(expr);
-      });
-      return;
+    switch (selectedObject.objectType) {
+      case ExObjectType.Attribute:
+        selectedObject.expr$.pipe(take(1)).subscribe((expr) => {
+          this.exprManager.getObject$(expr);
+        });
+        return;
+      case ExObjectType.Expr:
+        this.downExpr(selectedObject);
+        return;
+      default:
+        assertUnreachable(selectedObject);
     }
-
-    if (selectedObject.type === "CallExpr") {
-      if (selectedObject.args[0] === undefined) {
-        throw new Error("CallExpr must have at least 1 args");
-      }
-      selectedObject.args[0].pipe(take(1)).subscribe((arg) => {
-        this.selectedObject$.next(arg);
-      });
-      return;
-    }
-
-    if (selectedObject.type === "NumberExpr") {
-      return;
-    }
-
-    throw new Error("Unknown type");
   }
 
-  public up() {
-    const selectedObject = this.selectedObject$.value;
+  private downExpr(selectedObject: Expr) {
+    switch (selectedObject.exprType) {
+      case ExprType.NumberExpr:
+        return;
+      case ExprType.CallExpr:
+        if (selectedObject.args[0] === undefined) {
+          throw new Error("CallExpr must have at least 1 args");
+        }
+        const arg$ = selectedObject.args[0];
+        this.selectedObject$$.next(arg$);
+        return;
+      default:
+        assertUnreachable(selectedObject);
+    }
+  }
+
+  private up(selectedObject: Selectable) {
     if (selectedObject === null) {
       return;
     }
 
-    if (selectedObject.type === "Attribute") {
-      return;
+    switch (selectedObject.objectType) {
+      case ExObjectType.Attribute:
+        return;
+      case ExObjectType.Expr:
+        this.upExpr(selectedObject);
+        return;
+      default:
+        assertUnreachable(selectedObject);
     }
+  }
 
-    if (
-      selectedObject.type === "CallExpr" ||
-      selectedObject.type === "NumberExpr"
-    ) {
-      selectedObject.parent$.pipe(take(1)).subscribe((parent) => {
-        this.selectedObject$.next(parent);
-      });
-      return;
-    }
-
-    throw new Error("Unknown type");
+  private upExpr(selectedObject: Expr) {
+    const parent$ = this.exprManager.getParent$(selectedObject);
+    this.selectedObject$$.next(parent$);
   }
 }
