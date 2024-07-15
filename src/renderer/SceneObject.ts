d6b809e11ae561072a4f996441a6e8fef15af47d
Dylan Phan <dylan.phan@tixr.com>
2024-07-15 16:07:23 -0700

    checkpoint at 7/15/24 4:07P

--- src/renderer/SceneObject.ts
+++ src/renderer/SceneObject.ts
@@ -12,14 +12,26 @@ export interface SceneObject {
   destroy$: OBS<void>;
 }
 
-function attrToSceneCircle(attr: Attribute, ctx: MainContext): SceneObject {
-  return {
-    x$: attr.expr$.pipe(
-      map((expr) => {
-        const r = ctx.goModule.evalExpr(expr.id);
-        return r;
-      })
-    ),
-    destroy$: ctx.objectManager.getDestroy$(attr).pipe(map(() => {})),
-  };
+class SceneObjectManager {
+  public constructor(private readonly ctx: MainContext) {
+    ctx.objectFactory.onAttributeAdded$.subscribe((attr) => {
+      const sceneCircle = this.attrToSceneCircle(attr);
+      this.sceneObjectToPixi(sceneCircle);
+    });
+  }
+
+  public attrToSceneCircle(attr: Attribute): SceneObject {
+    return {
+      x$: attr.expr$.pipe(
+        map((expr) => {
+          const r = this.ctx.goModule.evalExpr(expr.id);
+          return r;
+        })
+      ),
+      destroy$: this.ctx.objectManager.getDestroy$(attr).pipe(map(() => {})),
+    };
+  }
+
+  public sceneObjectToPixi(sceneObject: SceneObject) {
+  }
 }
