dcc0705eadfed8b0ddd805c1218e2a3f41b75eaa
Dylan Phan <dylan.phan@tixr.com>
2024-07-16 20:39:53 -0700

    checkpoint at 7/16/24 8:39P

--- src/scene/SceneMain.ts
+++ src/scene/SceneMain.ts
@@ -1,11 +1,11 @@
 import MainContext from "../MainContext";
 import PixiFactory from "./PixiFactory";
 import { SceneContext } from "./SceneContext";
-import { SceneObjectManager } from "./SceneObjectManager";
+import { SceneManager } from "./SceneObjectManager";
 
 export class SceneMain {
     constructor(ctx: MainContext, pixiFactory: PixiFactory) {
         const sceneCtx = new SceneContext(ctx, pixiFactory);
-        new SceneObjectManager(sceneCtx);
+        new SceneManager(sceneCtx);
     }
 }
--- src/scene/SceneObjectManager.ts
+++ src/scene/SceneObjectManager.ts
@@ -9,27 +9,21 @@ import { SceneAttribute } from "../SceneAttribute";
 import { OBS } from "../utils/Utils";
 import { SceneContext } from "./SceneContext";
 
-interface SceneObjectAttribute {
+interface SceneObject {
   value$: OBS<number>;
   sceneAttribute: SceneAttribute;
 }
 
-export interface SceneObject {
-  x: SceneObjectAttribute;
-  destroy$: OBS<void>;
-}
-
 // todp: rename to SceneManager
-export class SceneObjectManager {
+export class SceneManager {
   private readonly tick$ = interval(1000);
 
   public constructor(private readonly ctx: SceneContext) {
     // todp
-    
-    // ctx.mainCtx.objectManager.onAttributeAdded$.subscribe((attr) => {
-    //   const sceneObject = this.attrToSceneCircle(attr);
-    //   this.sceneObjectToPixi(sceneObject);
-    // });
+    ctx.mainCtx.onSceneAttributeAdded$.subscribe((attr) => {
+      const sceneObject = this.attrToSceneCircle(attr);
+      this.sceneObjectToPixi(sceneObject);
+    });
   }
 
   @loggedMethod
@@ -37,19 +31,15 @@ export class SceneObjectManager {
   private attrToSceneCircle(sceneAttr: SceneAttribute): SceneObject {
     const logger = Logger.logger();
     return {
-      x: {
-        value$: this.tick$.pipe(
-          withLatestFrom(sceneAttr.attribute.expr$),
-          switchMap(([_, expr]) => {
-            logger.log("switchMap", expr.id);
-            const result$ = this.ctx.mainCtx.goBridge.evalExpr$(expr);
-            return result$;
-          })
-        ),
-        sceneAttribute: sceneAttr,
-      },
-      
-      destroy$: sceneAttr.attribute.destroy$,
+      value$: this.tick$.pipe(
+        withLatestFrom(sceneAttr.attribute.expr$),
+        switchMap(([_, expr]) => {
+          logger.log("switchMap", expr.id);
+          const result$ = this.ctx.mainCtx.goBridge.evalExpr$(expr);
+          return result$;
+        })
+      ),
+      sceneAttribute: sceneAttr,
     };
   }
 
@@ -59,14 +49,17 @@ export class SceneObjectManager {
     const logger = Logger.logger();
     Logger.logCallstack();
     const pixiObject = this.ctx.pool.takeCircle();
-    sceneObject.x.value$.subscribe((x) => {
+
+    const sceneAttribute = sceneObject.sceneAttribute;
+
+    sceneObject.value$.subscribe((value) => {
       pixiObject.visible = true;
-      sceneObject.x.sceneAttribute.proto.pixiSetter(pixiObject, x);
+      sceneAttribute.proto.pixiSetter(pixiObject, value);
       pixiObject.scale.x = 50;
       pixiObject.scale.y = 50;
     });
 
-    sceneObject.destroy$.subscribe(() => {
+    sceneAttribute.attribute.destroy$.subscribe(() => {
       logger.log("destroying pixiObject");
       pixiObject.visible = false;
       this.ctx.pool.releaseCircle(pixiObject);
