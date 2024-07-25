ec668c8f97a2c463705eb0b9afac8511de21d7d1
Dylan Phan <dylan.phan@tixr.com>
2024-07-24 17:49:27 -0700

    checkpoint at 7/24/24 5:49P

--- src/utils/views/NavMenuView.svelte
+++ src/utils/views/NavMenuView.svelte
@@ -1,28 +1,37 @@
 <script lang="ts">
   import { Window } from "src/main-context/MainViewContext";
   import NavItemView from "./NavItemView.svelte";
-  export let ctx: any;
+  import MainContext from "src/main-context/MainContext";
+  import { map } from "rxjs";
+  export let ctx: MainContext;
   let clazz: string = "";
   export { clazz as class };
+
+  const focused$ = ctx.focusManager
+    .getFocus$()
+    .pipe(map((focus) => focus.type === "ProjectNav"));
 </script>
 
-<ul class="menu bg-base-200 p-4 h-full {clazz}">
-  <div class="menu-title">
-    <span>Hello World</span>
+<ul class="menu bg-base-200 h-full {clazz}">
+  <div class="p-2" class:ring={$focused$}>
+    <div class="menu-title">
+      <span>Hello World</span>
+    </div>
+    <li>
+      <NavItemView {ctx} window={Window.ProjectEditor}>Editor</NavItemView>
+    </li>
+    <li>
+      <NavItemView {ctx} window={Window.ProjectComponentList}
+        >Components</NavItemView
+      >
+    </li>
+    <li>
+      <NavItemView {ctx} window={Window.ProjectFunctionList}
+        >Functions</NavItemView
+      >
+    </li>
   </div>
-  <li>
-    <NavItemView {ctx} window={Window.ProjectEditor}>Editor</NavItemView>
-  </li>
-  <li>
-    <NavItemView {ctx} window={Window.ProjectComponentList}
-      >Components</NavItemView
-    >
-  </li>
-  <li>
-    <NavItemView {ctx} window={Window.ProjectFunctionList}
-      >Functions</NavItemView
-    >
-  </li>
+
   <div class="divider my-0"></div>
   <div class="menu-title">Library</div>
   <li>
