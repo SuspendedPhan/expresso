aa14949951ab8a5e3f76168631fe539ee513cb2c
Dylan Phan <dylan.phan@tixr.com>
2024-07-24 19:34:45 -0700

    checkpoint at 7/24/24 7:34P

--- src/utils/views/NavMenuView.svelte
+++ src/utils/views/NavMenuView.svelte
@@ -5,7 +5,11 @@
   export let ctx: MainContext;
 </script>
 
-<ul class:menu={true} class="bg-base-200 h-full">
+<ul class="bg-base-200 h-full menu">
+  <div class="px-4 py-2">
+    <div class="text-lg w-max">Hello World</div>
+  </div>
+
   {#each ctx.viewCtx.navSections as section}
     <div class="divider m-0"></div>
     <div class="menu-title">{section.title}</div>
