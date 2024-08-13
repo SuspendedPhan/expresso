3cb64eef778728e346f88307c2db2adb726ba4da
Dylan Phan <dylan.phan@tixr.com>
2024-08-13 14:15:29 -0700

    checkpoint at 8/13/24 2:15P

--- src/utils/utils/CommandCard.svelte
+++ src/utils/utils/CommandCard.svelte
@@ -8,7 +8,8 @@
 
 {#if $visible$}
   <div
-    class="fixed left-0 top-0 right-0 bottom-0 flex justify-center items-center pointer-events-none"
+    class="fixed left-0 right-0 flex justify-center items-center pointer-events-none"
+    style:top="10vh"
   >
     <FocusView focused={true} class="ex-card p-4 min-w-96">
       <div class="mb-4">{data.title}</div>
