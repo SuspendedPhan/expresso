25175249641a5fc7088339603e54461603547a92
Dylan Phan <dylan.phan@tixr.com>
2024-08-22 16:40:57 -0700

    checkpoint at 8/22/24 4:40P

--- src/utils/views/SettingsView.svelte
+++ src/utils/views/SettingsView.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
+  import { get, set } from "idb-keyval";
+  import { log5 } from "src/utils/utils/Log3";
   import MainPane from "src/utils/views/MainPane.svelte";
+
+  const log55 = log5("SettingsView.svelte");
+
+  async function setup() {
+    const dirHandle = await get("dirHandle");
+    if (dirHandle !== undefined) {
+      log55.debug2("dirHandle", dirHandle);
+
+      for await (const entry of dirHandle.values()) {
+        console.log(entry.kind, entry.name);
+      }
+    }
+  }
+
+  setup();
 
   async function chooseDir() {
     const dirHandle = await self.showDirectoryPicker();
     for await (const entry of dirHandle.values()) {
       console.log(entry.kind, entry.name);
     }
+    set("dirHandle", dirHandle);
   }
 </script>
 
