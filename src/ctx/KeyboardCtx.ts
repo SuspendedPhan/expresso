f0028f2e7c4ac2e28839b9c2395142c1a1550cd4
Dylan Phan <dylan.phan@tixr.com>
2024-09-06 14:29:37 -0700

    11 checkpoint at 9/6/24 2:29P

--- src/ctx/KeyboardCtx.ts
+++ src/ctx/KeyboardCtx.ts
@@ -1,5 +1,11 @@
 import { Context, Effect, Layer } from "effect";
-import type { DexEffectSuccess } from "src/utils/utils/Utils";
+import { switchMap, of, fromEvent, filter, map } from "rxjs";
+import { FocusCtx } from "src/ctx/FocusCtx";
+import type { DexEffectSuccess, OBS } from "src/utils/utils/Utils";
+
+export enum KeyboardResult {
+  OutOfScope,
+}
 
 export class KeyboardCtx extends Context.Tag("KeyboardCtx")<
   KeyboardCtx,
@@ -7,7 +13,52 @@ export class KeyboardCtx extends Context.Tag("KeyboardCtx")<
 >() {}
 
 const ctxEffect = Effect.gen(function* () {
+  const focusCtx = yield* FocusCtx;
+
+  function onKeydown$<T>(keys: string, data$: OBS<T | false>) {
+    return onKeydown2$({ keys, data$ });
+  }
+
+  function onKeydown2$<T>(data: {
+    keys: string;
+    data$: OBS<T | false>;
+    preventDefault?: boolean;
+  }) {
+    const { keys, data$, preventDefault = false } = data;
     
+    const keyArr = keys.split(",");
+
+    return data$.pipe(
+      switchMap((data) => {
+        if (data === false) {
+          return of();
+        }
+
+        return fromEvent<KeyboardEvent>(window, "keydown").pipe(
+          filter((event) => {
+            const matches = keyArr.includes(event.key);
+            if (matches) {
+              if (preventDefault) {
+                event.preventDefault();
+              }
+            }
+            return matches;
+          }),
+          map(() => data)
+        );
+      })
+    );
+  }
+
+  return {
+    onKeydown$,
+    onKeydown2$,
+    registerCancel: <T>(data$: OBS<T | false>) => {
+      onKeydown$("Escape", data$).subscribe(() => {
+        focusCtx.popFocus();
+      });
+    },
+  };
 });
 
 export const KeyboardCtxLive = Layer.effect(
