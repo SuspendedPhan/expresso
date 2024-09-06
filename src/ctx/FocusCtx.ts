192c0cb71f865848d13aa81d25908c369af4268a
Dylan Phan <dylan.phan@tixr.com>
2024-09-06 10:40:52 -0700

    10-effect checkpoint at 9/6/24 10:40P

---
+++ src/ctx/FocusCtx.ts
@@ -0,0 +1,87 @@
+import { Context, Effect, Layer } from "effect";
+import { BehaviorSubject, map } from "rxjs";
+import type { OBS, SUB } from "src/utils/utils/Utils";
+import { type UnionOf } from "unionize";
+import { FocusKind } from "./FocusKind";
+
+// const log55 = log5("Focus.ts");
+
+export type Focus = UnionOf<typeof FocusKind>;
+
+export enum Hotkeys {
+  Down = "ArrowDown,s",
+  Up = "ArrowUp,w",
+  Left = "ArrowLeft,a",
+  Right = "ArrowRight,d",
+}
+
+export class FocusCtx extends Context.Tag("FocusCtx")<
+  FocusCtx,
+  {
+    focus$: SUB<Focus>;
+    popFocus(): void;
+    focusOrFalse$<T extends Focus>(
+      predicate: (focus: Focus) => focus is T
+    ): OBS<T | false>;
+
+    mapFocus$<T>(mapperFn: (focus: Focus) => T): OBS<T>;
+    register(): void;
+  }
+>() {}
+
+export const FocusCtxLive = Layer.effect(
+  FocusCtx,
+  Effect.gen(function* () {
+    const focus$ = new BehaviorSubject<Focus>(FocusKind.None());
+    const focusStack = new Array<Focus>();
+
+    return {
+      focus$: focus$,
+      popFocus() {
+        focusStack.pop();
+        focus$.next(focusStack[focusStack.length - 1] ?? FocusKind.None());
+      },
+
+      focusOrFalse$<T extends Focus>(
+        predicate: (focus: Focus) => focus is T
+      ): OBS<T | false> {
+        return focus$.pipe(
+          map((focus) => {
+            const result = predicate(focus);
+            return result === false ? false : focus;
+          })
+        );
+      },
+
+      mapFocus$<T>(mapperFn: (focus: Focus) => T) {
+        return focus$.pipe(map(mapperFn));
+      },
+
+      register() {
+        // EditorFocusFuncs.register(ctx);
+        // ExObjectFocusFuncs.register(ctx);
+        // ExprFocusFuncs.register(ctx);
+        document.addEventListener("keydown", async (event: KeyboardEvent) => {
+          if (event.target !== null && "tagName" in event.target) {
+            if (event.target.tagName === "INPUT") {
+              return;
+            }
+          }
+
+          switch (event.key) {
+            case Hotkeys.Down:
+            case Hotkeys.Up:
+            case Hotkeys.Left:
+            case Hotkeys.Right:
+              event.preventDefault();
+              break;
+          }
+        });
+
+        document.addEventListener("mousedown", async () => {
+          this.popFocus();
+        });
+      },
+    };
+  })
+);
