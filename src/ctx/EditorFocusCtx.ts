cbf8eae2ce3ef663c0d61c1cb56ddfdf6c3a5fa9
Dylan Phan <dylan.phan@tixr.com>
2024-09-06 14:41:41 -0700

    11 checkpoint at 9/6/24 2:41P

--- src/ctx/EditorFocusCtx.ts
+++ src/ctx/EditorFocusCtx.ts
@@ -1,5 +1,8 @@
 import { Context, Layer, Effect } from "effect";
+import { combineLatestWith, map, firstValueFrom } from "rxjs";
+import { FocusCtx } from "src/ctx/FocusCtx";
 import { KeyboardCtx } from "src/ctx/KeyboardCtx";
+import { DexWindow } from "src/main-context/MainViewContext";
 import type { DexEffectSuccess } from "src/utils/utils/Utils";
 
 export class EditorFocusCtx extends Context.Tag("EditorFocusCtx")<
@@ -8,17 +11,65 @@ export class EditorFocusCtx extends Context.Tag("EditorFocusCtx")<
 >() {}
 
 const ctxEffect = Effect.gen(function* () {
-    const keyboardCtx = yield* KeyboardCtx;
+  const keyboardCtx = yield* KeyboardCtx;
+  const focusCtx = yield* FocusCtx;
+  const viewCtx = yield* ViewCtx;
 
-    return {
-        register() {
+  return {
+    register() {
 
-        }
-    };
+      keyboardCtx
+        .onKeydown$(
+          "n",
+          ctx.viewCtx.activeWindowEqualTo$(DexWindow.ProjectEditor).pipe(
+            combineLatestWith(focusCtx.mapFocus$(FocusKind.is.None)),
+            map(([activeWindow, noneFocus]) => {
+              return activeWindow && noneFocus;
+            })
+          )
+        )
+        .subscribe(async () => {
+          const exItem = await firstValueFrom(
+            ctx.exObjectFocusCtx.exItemFocus$
+          );
+          focusCtx.setFocus(
+            FocusKind.EditorNewActions({
+              exItem: exItem === false ? null : exItem,
+            })
+          );
+        });
+
+      const focusIsEditorNewActions$ = focusCtx.mapFocus$((focus) => {
+        return FocusKind.is.EditorNewActions(focus) ? focus : false;
+      });
+
+      ctx.viewCtx.commandCardCtx.addCommandCard({
+        title: "Editor Commands",
+        commands: ["New Object", "New Project"],
+        visible$: focusIsEditorNewActions$.pipe(
+          map((focus) => focus !== false)
+        ),
+      });
+
+      keyboardCtx
+        .onKeydown$("p", focusIsEditorNewActions$)
+        .subscribe(async () => {
+          const library = await firstValueFrom(ctx.library$);
+          library.addProjectBlank();
+          focusCtx.popFocus();
+        });
+
+      keyboardCtx
+        .onKeydown$("o", focusIsEditorNewActions$)
+        .subscribe(async () => {
+          const project = await ctx.projectCtx.getCurrentProjectProm();
+          project.addRootExObjectBlank();
+          focusCtx.popFocus();
+        });
+
+      keyboardCtx.registerCancel(focusIsEditorNewActions$);
+    },
+  };
 });
 
-export const EditorFocusCtxLive = Layer.effect(
-  EditorFocusCtx,
-  ctxEffect
-);
-
+export const EditorFocusCtxLive = Layer.effect(EditorFocusCtx, ctxEffect);
---
+++ src/ctx/ViewCtx.ts
@@ -0,0 +1,114 @@
+import { Context, Effect, Layer } from "effect";
+import { BehaviorSubject, map } from "rxjs";
+import type { ElementLayout } from "src/utils/layout/ElementLayout";
+import type { NavItem, NavSection } from "src/utils/utils/Nav";
+import type { DexEffectSuccess } from "src/utils/utils/Utils";
+
+export enum DexWindow {
+  ProjectEditor,
+  ProjectComponents,
+  ProjectFunctionList,
+  LibraryProjectList,
+  LibraryComponentList,
+  LibraryFunctionList,
+  TestView,
+  Settings,
+}
+
+export enum ViewMode {
+  Default,
+  MainWindowMaximized,
+  CanvasWindowMaximized,
+}
+
+export class ViewCtx extends Context.Tag("ViewCtx")<
+  ViewCtx,
+  DexEffectSuccess<typeof ctxEffect>
+>() {}
+
+const ctxEffect = Effect.gen(function* () {
+  const section0: NavSection = {
+    title: "Project",
+    navItems: [],
+  };
+
+  const navItems0: NavItem[] = [
+    {
+      label: "Editor",
+      window: DexWindow.ProjectEditor,
+      iconClasses: "fa-solid fa-file",
+      section: section0,
+    },
+    {
+      label: "Components",
+      window: DexWindow.ProjectComponents,
+      iconClasses: "fa-solid fa-cube",
+      section: section0,
+    },
+    {
+      label: "Functions",
+      window: DexWindow.ProjectFunctionList,
+      iconClasses: "fa-solid fa-code",
+      section: section0,
+    },
+    // { label: "TestView", window: DexWindow.TestView, iconClasses: "fa-solid fa-file", section: section0 },
+  ];
+
+  const section1: NavSection = {
+    title: "Library",
+    navItems: [],
+  };
+
+  const navItems1: NavItem[] = [
+    {
+      label: "Projects",
+      window: DexWindow.LibraryProjectList,
+      iconClasses: "fa-solid fa-file",
+      section: section1,
+    },
+    {
+      label: "Components",
+      window: DexWindow.LibraryComponentList,
+      iconClasses: "fa-solid fa-cube",
+      section: section1,
+    },
+    {
+      label: "Functions",
+      window: DexWindow.LibraryFunctionList,
+      iconClasses: "fa-solid fa-code",
+      section: section1,
+    },
+  ];
+
+  const section2: NavSection = {
+    title: "",
+    navItems: [],
+  };
+
+  const navItems2: NavItem[] = [
+    {
+      label: "Settings",
+      window: DexWindow.Settings,
+      iconClasses: "fa-solid fa-gears",
+      section: section2,
+    },
+  ];
+
+  (section0 as any).navItems = navItems0;
+  (section1 as any).navItems = navItems1;
+  (section2 as any).navItems = navItems2;
+
+  return {
+    exObjectLayouts$: new BehaviorSubject<ElementLayout[]>([]),
+    activeWindow$: new BehaviorSubject<DexWindow>(DexWindow.ProjectEditor),
+    navCollapsed$: new BehaviorSubject<boolean>(false),
+    navSections: [section0, section1, section2],
+    viewMode$: ViewMode.Default,
+
+    activeWindowEqualTo$(window: DexWindow) {
+        return this.activeWindow$.pipe(map((w) => w === window));
+      }
+  };
+});
+
+export const ViewCtxLive = Layer.effect(ViewCtx, ctxEffect);
