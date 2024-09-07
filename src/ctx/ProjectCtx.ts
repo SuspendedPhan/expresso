7817ca2e76621ef34f69275a1554f6f3804c2afe
Dylan Phan <dylan.phan@tixr.com>
2024-09-07 15:51:12 -0700

    18 checkpoint at 9/7/24 3:51P

--- src/ctx/ProjectCtx.ts
+++ src/ctx/ProjectCtx.ts
@@ -1,7 +1,6 @@
 import { Context, Effect, Layer } from "effect";
-import { firstValueFrom, map, ReplaySubject, switchMap } from "rxjs";
-import { ExFuncFactory2 } from "src/ex-object/ExFunc";
-import { Project } from "src/ex-object/Project";
+import { firstValueFrom, switchMap } from "rxjs";
+import { LibraryProjectCtx } from "src/ctx/LibraryProjectCtx";
 import type { DexEffectSuccess } from "src/utils/utils/Utils";
 
 export class ProjectCtx extends Context.Tag("ProjectCtx")<
@@ -10,7 +9,6 @@ export class ProjectCtx extends Context.Tag("ProjectCtx")<
 >() {}
 
 const ctxEffect = Effect.gen(function* () {
-  const currentProject$ = new ReplaySubject<Project>(1);
   const libraryProjectCtx = yield* LibraryProjectCtx;
   const activeLibraryProject$ = libraryProjectCtx.activeLibraryProject$;
   return {
@@ -21,82 +19,14 @@ const ctxEffect = Effect.gen(function* () {
     get activeProject() {
       return Effect.gen(function* () {
         const project = yield* Effect.promise(() =>
-          firstValueFrom(activeLibraryProject$.pipe(switchMap((lp) => lp.project$)))
+          firstValueFrom(
+            activeLibraryProject$.pipe(switchMap((lp) => lp.project$))
+          )
         );
         return project;
       });
-    },
-
-    getOrdinalProm() {
-      return firstValueFrom(
-        currentProject$.pipe(map(Project.getAndIncrementOrdinal))
-      );
-    },
-    getCurrentProjectProm() {
-      return firstValueFrom(currentProject$);
-    },
-    addRootExObjectBlank() {
-      return firstValueFrom(
-        currentProject$.pipe(map(Project.addRootExObjectBlank))
-      );
-    },
-    addExFuncBlank() {
-      return firstValueFrom(currentProject$.pipe(map(ExFuncFactory2.Custom)));
-    },
-    addComponentBlank() {
-      return firstValueFrom(
-        currentProject$.pipe(map(Project.addComponentBlank))
-      );
     },
   };
 });
 
 export const ProjectCtxLive = Layer.effect(ProjectCtx, ctxEffect);
-
-// // Create a tag for the ProjectCtx service
-// class ProjectCtx extends Context.Tag("ProjectCtx")<
-//   ProjectCtx,
-//   {
-//     getOrdinalProm(): Promise<number>;
-//     getCurrentProjectProm(): Promise<Project>;
-//     addRootExObjectBlank(): Promise<void>;
-//     addExFuncBlank(): Promise<void>;
-//     addComponentBlank(): Promise<void>;
-//   }
-// >() {}
-
-// const ProjectCtxLive = Layer.succeed(
-//   ProjectCtx,
-//   Effect.gen(function* () {
-//     const currentProject$ = new ReplaySubject<Project>(1);
-//     return {
-
-//     };
-//   })
-// );
-
-// export function createProjectContext(ctx: MainContext) {
-//   return {
-//     currentProject$: ctx.projectManager.currentProject$,
-//     async getOrdinalProm() {
-//       const project = await firstValueFrom(ctx.projectManager.currentProject$);
-//       return Project.getAndIncrementOrdinal(project);
-//     },
-//     async getCurrentProjectProm() {
-//       return firstValueFrom(ctx.projectManager.currentProject$);
-//     },
-//     async addRootExObjectBlank() {
-//       const project = await firstValueFrom(ctx.projectManager.currentProject$);
-//       return project.addRootExObjectBlank();
-//     },
-//     async addExFuncBlank() {
-//       const project = await firstValueFrom(ctx.projectManager.currentProject$);
-//       const exFunc = await ExFuncFactory2.Custom(ctx, {});
-//       return project.addCustomExFunc(exFunc);
-//     },
-//     async addComponentBlank() {
-//       const project = await firstValueFrom(ctx.projectManager.currentProject$);
-//       return Project.addComponentBlank(ctx, project);
-//     },
-//   };
-// }
