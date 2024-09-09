34b614a96a77be6ac94818649d104e661d783c6f
Dylan Phan <dylan.phan@tixr.com>
2024-09-08 21:29:52 -0700

    20 checkpoint at 9/8/24 9:29P

--- src/utils/utils/Main.ts
+++ src/utils/utils/Main.ts
@@ -1,22 +1,6 @@
 import { first, map, type Observable } from "rxjs";
-import MainContext from "src/main-context/MainContext";
-import type GoModule from "src/utils/utils/GoModule";
 import GoModuleLoader from "src/utils/utils/GoModuleLoader";
 
 export default class Main {
-  public readonly ctx: MainContext;
-
-  public static setup$(): Observable<Main> {
-    return GoModuleLoader.get$().pipe(
-      first(),
-      map((goModule) => {
-        return new Main(goModule);
-      })
-    );
-  }
-
-  public constructor(goModule: GoModule) {
-    const ctx = new MainContext(goModule);
-    this.ctx = ctx;
-  }
+  
 }
