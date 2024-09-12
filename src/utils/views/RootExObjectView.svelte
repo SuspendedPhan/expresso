b342c07c27b3f7cde06bbd2b1c5668c6cf547f0f
Dylan Phan <dylan.phan@tixr.com>
2024-09-11 20:26:36 -0700

    root-ex-centered checkpoint at 9/11/24 8:26P

--- src/utils/views/RootExObjectView.svelte
+++ src/utils/views/RootExObjectView.svelte
@@ -5,12 +5,11 @@
   import type { ElementLayout } from "src/utils/layout/ElementLayout";
   import { createExObjectLayout } from "src/utils/layout/ExObjectLayout";
   import { DexRuntime } from "src/utils/utils/DexRuntime";
+  import { log5 } from "src/utils/utils/Log5";
   import ExObjectView from "src/utils/views/ExObjectView.svelte";
   import FlexContainer from "src/utils/views/FlexContainer.svelte";
   import { onMount, tick } from "svelte";
   import TreeView from "../layout/TreeView.svelte";
-  import { log5 } from "src/utils/utils/Log5";
-  import { Deferred } from "effect";
 
   const log55 = log5("RootExObjectView.svelte");
 
@@ -19,23 +18,29 @@
   export { clazz as class };
 
   let elementLayout: ElementLayout;
-
   let element: HTMLElement;
-
   let sensor: ResizeSensor;
 
   onMount(() => {
     log55.debug("onMount");
-    DexRuntime.runPromise(createExObjectLayout(exObject)).then((layout) => {
-      log55.debug("layout", layout);
+    DexRuntime.runPromise(createExObjectLayout(exObject)).then(
+      async (layout) => {
+        log55.debug("layout", layout);
 
-      elementLayout = layout;
-      sensor = new ResizeSensor(element, () => {
-        tick().then(() => {
-          elementLayout.recalculate();
+        elementLayout = layout;
+        log55.debug("element", element);
+
+        // Wait for element to be set
+        await tick();
+        log55.debug("element2", element);
+
+        sensor = new ResizeSensor(element, () => {
+          tick().then(() => {
+            elementLayout.recalculate();
+          });
         });
-      });
-    });
+      }
+    );
 
     return () => {
       sensor.detach();
