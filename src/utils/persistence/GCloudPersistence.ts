8ca5b0c54af3463bcf1c648e22dd5f5c61fc3e9a
Dylan Phan <dylan.phan@tixr.com>
2024-09-16 20:06:45 -0700

    delete-persistence checkpoint at 9/16/24 8:06P

--- src/utils/persistence/GCloudPersistence.ts
+++ src/utils/persistence/GCloudPersistence.ts
@@ -1,7 +1,14 @@
-import { getBlob, getStorage, listAll, ref, uploadString } from "firebase/storage";
+import {
+  getBlob,
+  getStorage,
+  listAll,
+  ref,
+  uploadString,
+} from "firebase/storage";
 import { log5 } from "src/utils/utils/Log5";
 import Firebase from "./Firebase";
 import type { PersistService } from "./Persistence";
+import { Effect } from "effect";
 
 const log55 = log5("GCloudPersistence.ts");
 
@@ -10,16 +17,16 @@ const storage = getStorage();
 
 export default class GCloudPersistence implements PersistService {
   async readFile(path: string): Promise<string | null> {
-    log55.debug("Reading file", path);
+    log55.debug("readFile.start", path);
     const storageRef = ref(storage, path);
     let blob;
     try {
-        blob = await getBlob(storageRef);
+      blob = await getBlob(storageRef);
     } catch (e: any) {
-        if (e.code === 'storage/object-not-found') {
-            return null;
-        }
-        throw e;
+      if (e.code === "storage/object-not-found") {
+        return null;
+      }
+      throw e;
     }
 
     const text = await blob.text();
@@ -27,34 +34,24 @@ export default class GCloudPersistence implements PersistService {
   }
 
   async writeFile(path: string, content: string): Promise<void> {
-    log55.debug("Writing file", path);
+    log55.debug("writeFile.start", path);
     const storageRef = ref(storage, path);
 
     // Raw string is the default if no format is provided
     await uploadString(storageRef, content);
-    log55.debug("Uploaded a blob or file");
+    log55.debug("writeFile.end");
   }
 
-  async listFiles(path: string) {
-    log55.debug("Listing files", path);
-    const storageRef = ref(storage, path);
-  
-    try {
-      const result = await listAll(storageRef);
-
-      log55.debug("Listed files", result);
-  
-      // List all files
-      result.items.forEach((itemRef) => {
-        log55.debug("File", itemRef.fullPath);
-      });
-  
-      // List all directories (if any)
-      result.prefixes.forEach((folderRef) => {
-        log55.debug("Directory", folderRef.fullPath);
-      });
-    } catch (error) {
-      console.error('Error listing files:', error);
-    }
+  listFiles(path: string): Effect.Effect<string[], Error, never> {
+    return Effect.gen(function* () {
+      log55.debug("listFiles.start", path);
+      
+      const storageRef = ref(storage, path);
+      const result = yield* Effect.promise(() => listAll(storageRef));
+      log55.debug("listFiles.end", result);
+      
+      // We can access result.prefixes if needed
+      return result.items.map((itemRef) => itemRef.name);
+    });
   }
 }
