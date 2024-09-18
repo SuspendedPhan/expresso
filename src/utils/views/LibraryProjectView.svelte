22a69b6e5775e29f4ee86d1ec98d3d59ee6295fc
Dylan Phan <dylan.phan@tixr.com>
2024-09-17 20:01:17 -0700

    project-list checkpoint at 9/17/24 8:01P

--- src/utils/views/LibraryProjectView.svelte
+++ src/utils/views/LibraryProjectView.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { of } from "rxjs";
-  import type { LibraryProject } from "src/library/LibraryProject";
+  import type { LibraryProject } from "src/ex-object/LibraryProject";
+  import ExObjectButton from "src/utils/views/ExObjectButton.svelte";
 
   export let project: LibraryProject;
   const selected$ = of(false);
@@ -9,4 +10,5 @@
 <tr class:ring={$selected$}>
   <td>{project.id}</td>
   <td>{project.name}</td>
+  <td><ExObjectButton>Open</ExObjectButton></td>
 </tr>
