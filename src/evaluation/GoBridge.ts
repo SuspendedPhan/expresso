5cffdcdfaf16b970bd969f5eec15b3b90fa862e8
Dylan Phan <dylan.phan@tixr.com>
2024-09-23 20:10:13 -0700

    call-expr-canvas checkpoint at 9/23/24 8:10P

--- src/evaluation/GoBridge.ts
+++ src/evaluation/GoBridge.ts
@@ -61,7 +61,9 @@ const ctxEffect = Effect.gen(function* () {
   yield* Effect.forkDaemon(
     Stream.runForEach(exObjectAdded, (exObject) => {
       return Effect.gen(function* () {
-        log55.debug("Adding ExObject to Go: received from upstream: ex-object has been added");
+        log55.debug(
+          "Adding ExObject to Go: received from upstream: ex-object has been added"
+        );
         return yield* goModuleCtx.withGoModule((goModule) => {
           return Effect.gen(function* () {
             log55.debug2("Go: Adding Object" + ++exObjectCounter);
@@ -89,7 +91,9 @@ const ctxEffect = Effect.gen(function* () {
   yield* Effect.forkDaemon(
     Stream.runForEach(propertyAdded, (property) => {
       return Effect.gen(function* () {
-        log55.debug("Adding Property to Go: received from upstream: property has been added");
+        log55.debug(
+          "Adding Property to Go: received from upstream: property has been added"
+        );
         return yield* goModuleCtx.withGoModule((goModule) => {
           return Effect.gen(function* () {
             log55.debug2("Go: Adding Property " + ++propertyCounter);
@@ -116,46 +120,62 @@ const ctxEffect = Effect.gen(function* () {
   yield* Effect.forkDaemon(
     Stream.runForEach(exprAdded, (expr) => {
       return Effect.gen(function* () {
-        log55.debug("Adding Expr to Go: received from upstream: expr has been added");
+        log55.debug(
+          "Adding Expr to Go: received from upstream: expr has been added"
+        );
         return yield* goModuleCtx.withGoModule((goModule) => {
           return Effect.gen(function* () {
             // log55.debug2("Go: Adding Expr " + ++exprCounter);
             log55.debug2(`Go: Adding Expr ${++exprCounter} ${expr.type}`);
 
-            matcher(expr)
+            yield* matcher(expr)
               .when(ExprFactory.Number, (expr) => {
-                log55.debug("GoModule: Creating NumberExpr", expr.id);
-                goModule.NumberExpr.create(expr.id);
-                goModule.NumberExpr.setValue(expr.id, expr.value);
+                return Effect.gen(function* () {
+                  log55.debug("GoModule: Creating NumberExpr", expr.id);
+                  goModule.NumberExpr.create(expr.id);
+                  goModule.NumberExpr.setValue(expr.id, expr.value);
+                });
               })
               .when(ExprFactory.Reference, (expr_) => {
-                log55.debug("GoModule: Creating ReferenceExpr", expr_.id);
-                assert(expr_.target !== null);
-                goModule.ReferenceExpr.create(
-                  expr_.id,
-                  expr_.target.id,
-                  expr_.target.type
-                );
-              })
-              .when(ExprFactory.Call, (expr) => {
-                log55.debug("GoModule: Creating CallExpr", expr.id);
-                goModule.CallExpr.create(expr.id);
-                Stream.runForEach(expr.args.itemStream, (args) => {
-                  return goModuleCtx.withGoModule((goModule_) =>
-                    Effect.gen(function* () {
-                      const arg0 = args[0];
-                      const arg1 = args[1];
-
-                      if (arg0 === undefined || arg1 === undefined) {
-                        console.error("CallExpr must have 2 args");
-                        return;
-                      }
-
-                      goModule_.CallExpr.setArg0(expr.id, arg0.id);
-                      goModule_.CallExpr.setArg1(expr.id, arg1.id);
-                    })
+                return Effect.gen(function* () {
+                  log55.debug("GoModule: Creating ReferenceExpr", expr_.id);
+                  assert(expr_.target !== null);
+                  goModule.ReferenceExpr.create(
+                    expr_.id,
+                    expr_.target.id,
+                    expr_.target.type
                   );
                 });
+              })
+              .when(ExprFactory.Call, (expr) => {
+                return Effect.forkDaemon(
+                  Effect.gen(function* () {
+                    log55.debug("GoModule: Creating CallExpr", expr.id);
+                    goModule.CallExpr.create(expr.id);
+                    yield* Effect.forkDaemon(
+                      Stream.runForEach(expr.args.itemStream, (args) => {
+                        log55.debug2(
+                          "Setting CallExpr args: received args from upstream"
+                        );
+                        return goModuleCtx.withGoModule((goModule_) =>
+                          Effect.gen(function* () {
+                            const arg0 = args[0];
+                            const arg1 = args[1];
+
+                            if (arg0 === undefined || arg1 === undefined) {
+                              console.error("CallExpr must have 2 args");
+                              return;
+                            }
+
+                            log55.debug2("Go: Setting CallExpr args");
+                            goModule_.CallExpr.setArg0(expr.id, arg0.id);
+                            goModule_.CallExpr.setArg1(expr.id, arg1.id);
+                          })
+                        );
+                      })
+                    );
+                  })
+                );
               })
               .complete();
           });
@@ -165,7 +185,6 @@ const ctxEffect = Effect.gen(function* () {
   );
 
   return {};
-
 
   // // Add ExObjects
   // yield* Effect.forkDaemon(
