8e761e7a115d69f785d382a33cc926f8e2f982e9
Dylan Phan <dylan.phan@tixr.com>
2024-10-03 17:07:51 -0700

    finished combobox checkpoint at 10/3/24 5:07P

--- src/utils/views/ComponentSelect.svelte
+++ src/utils/views/ComponentSelect.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
+  import type { ExObject } from "src/ex-object/ExObject";
+  import type { ComboboxOption } from "src/utils/views/Combobox";
   import Combobox from "src/utils/views/Combobox.svelte";
+
+  export let exObject: ExObject;
+
+  let query: string;
+  let options: ComboboxOption[];
+
+  function queryChanged(value: string) {
+    query = value;
+  }
+
+  function optionSelected(option: ComboboxOption) {}
 </script>
 
-<Combobox query={}/>
+<Combobox
+  {query}
+  {options}
+  on:queryChanged={queryChanged}
+  on:optionSelected={optionSelected}
+/>
