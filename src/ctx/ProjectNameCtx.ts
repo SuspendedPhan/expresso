c120f566536b4a04b476c9713ba824c824f2678d
Dylan Phan <dylan.phan@tixr.com>
2024-10-16 15:33:53 -0700

    focus2 first pass checkpoint at 10/16/24 3:33P

--- src/ctx/ProjectNameCtx.ts
+++ src/ctx/ProjectNameCtx.ts
@@ -1,15 +1,6 @@
-import { Effect, Layer, Ref, Stream } from "effect";
-import { BehaviorSubject } from "rxjs";
-import { LibraryProjectCtx } from "src/ctx/LibraryProjectCtx";
-import {
-  type NavProjectNameFocus,
-  NavProjectNameFocusFactory,
-} from "src/focus/NavFocus";
-import { EffectUtils } from "src/utils/utils/EffectUtils";
-import { log5 } from "src/utils/utils/Log5";
-import { isType } from "variant";
+import { Effect, Layer } from "effect";
 
-const log55 = log5("ProjectNameCtx.ts");
+// const log55 = log5("ProjectNameCtx.ts");
 
 export class ProjectNameCtx extends Effect.Tag("ProjectNameCtx")<
   ProjectNameCtx,
@@ -20,58 +11,58 @@ const ctxEffect = Effect.gen(function* () {
   return {
     createProjectNameFieldData() {
       return Effect.gen(function* () {
-        const projectName$ = new BehaviorSubject<string>("");
+        // const projectName$ = new BehaviorSubject<string>("");
 
-        const projectNameFieldData: FieldValueData = yield* Effect.gen(
-          function* () {
-            const libraryProjectCtx = yield* LibraryProjectCtx;
+        // const projectNameFieldData: FieldValueData = yield* Effect.gen(
+        //   function* () {
+        //     const libraryProjectCtx = yield* LibraryProjectCtx;
 
-            const projectNameFieldData =
-              yield* createTextFieldValueData<NavProjectNameFocus>({
-                value$: projectName$,
-                focusIsFn: isType(NavProjectNameFocusFactory),
-                createEditingFocusFn: (isEditing) =>
-                  NavProjectNameFocusFactory({ isEditing }),
-                filterFn: () => true,
-              });
-            yield* Effect.forkDaemon(
-              Stream.runForEach(projectNameFieldData.onInput, (value) => {
-                return Effect.gen(function* () {
-                  log55.debug("Updating project name: input");
-                  const vv = yield* libraryProjectCtx.activeLibraryProject;
-                  yield* Ref.set(vv.name, value);
-                });
-              })
-            );
-            return projectNameFieldData;
-          }
-        );
+        //     const projectNameFieldData =
+        //       yield* createTextFieldValueData<NavProjectNameFocus>({
+        //         value$: projectName$,
+        //         focusIsFn: isType(NavProjectNameFocusFactory),
+        //         createEditingFocusFn: (isEditing) =>
+        //           NavProjectNameFocusFactory({ isEditing }),
+        //         filterFn: () => true,
+        //       });
+        //     yield* Effect.forkDaemon(
+        //       Stream.runForEach(projectNameFieldData.onInput, (value) => {
+        //         return Effect.gen(function* () {
+        //           log55.debug("Updating project name: input");
+        //           const vv = yield* libraryProjectCtx.activeLibraryProject;
+        //           yield* Ref.set(vv.name, value);
+        //         });
+        //       })
+        //     );
+        //     return projectNameFieldData;
+        //   }
+        // );
 
-        yield* Effect.forkDaemon(Effect.gen(function* () {
-          log55.debug("Updating project name: start");
-          // TODO: leak
-          const libraryProjectCtx = yield* LibraryProjectCtx;
-          const activeLibraryProject = EffectUtils.obsToStream(
-            libraryProjectCtx.activeLibraryProject$
-          );
-          const name = Stream.flatMap(
-            activeLibraryProject,
-            (activeLibraryProject2) => {
-              log55.debug(
-                "Updating project name: active library project changed"
-              );
-              return activeLibraryProject2.name.changes;
-            },
-            { switch: true }
-          );
-          yield* Stream.runForEach(name, (name2) => {
-            return Effect.gen(function* () {
-              projectName$.next(name2);
-            });
-          });
-        }));
+        // yield* Effect.forkDaemon(Effect.gen(function* () {
+        //   log55.debug("Updating project name: start");
+        //   // TODO: leak
+        //   const libraryProjectCtx = yield* LibraryProjectCtx;
+        //   const activeLibraryProject = EffectUtils.obsToStream(
+        //     libraryProjectCtx.activeLibraryProject$
+        //   );
+        //   const name = Stream.flatMap(
+        //     activeLibraryProject,
+        //     (activeLibraryProject2) => {
+        //       log55.debug(
+        //         "Updating project name: active library project changed"
+        //       );
+        //       return activeLibraryProject2.name.changes;
+        //     },
+        //     { switch: true }
+        //   );
+        //   yield* Stream.runForEach(name, (name2) => {
+        //     return Effect.gen(function* () {
+        //       projectName$.next(name2);
+        //     });
+        //   });
+        // }));
 
-        return projectNameFieldData;
+        // return projectNameFieldData;
       });
     },
   };
--- src/focus/EditorFocus.ts
+++ src/focus/EditorFocus.ts
@@ -1,17 +1,9 @@
 import { Effect } from "effect";
-import { combineLatestWith, map } from "rxjs";
-import { KeyboardCtx } from "src/ctx/KeyboardCtx";
-import { LibraryCtx } from "src/ctx/LibraryCtx";
+import { map } from "rxjs";
 
-import { DexWindow, ViewCtx } from "src/ctx/ViewCtx";
 import { ExItem } from "src/ex-object/ExItem";
-import { ExObjectFactory2 } from "src/ex-object/ExObject";
-import { LibraryProjectFactory2 } from "src/ex-object/LibraryProject";
-import { Project } from "src/ex-object/Project";
-import { FocusFactory } from "src/focus/Focus";
 import { FocusCtx } from "src/focus/FocusCtx";
 import { CommandCardCtx } from "src/utils/utils/CommandCard";
-import { DexRuntime } from "src/utils/utils/DexRuntime";
 import { dexVariant, type DexVariantKind } from "src/utils/utils/VariantUtils4";
 import { isType, pass, type VariantOf } from "variant";
 
@@ -31,34 +23,32 @@ export type EditorFocusKind = DexVariantKind<typeof EditorFocusFactory>;
 export const EditorFocus = {
   register: Effect.gen(function* () {
     const focusCtx = yield* FocusCtx;
-    const keyboardCtx = yield* KeyboardCtx;
-    const viewCtx = yield* ViewCtx;
     const commandCardCtx = yield* CommandCardCtx;
 
-    keyboardCtx
-      .onKeydown$(
-        "n",
-        viewCtx.activeWindowEqualTo$(DexWindow.ProjectEditor).pipe(
-          combineLatestWith(focusCtx.mapFocus$(isType(FocusFactory.None))),
-          map(([activeWindow, noneFocus]) => {
-            return activeWindow && noneFocus;
-          })
-        )
-      )
-      .subscribe(async () => {
-        // const effect = Effect.gen(function* () {
-        //     const exItem$ = yield* ExItemFocus.exItemBasicFocus$;
-        //     const exItem = yield* EffectUtils.firstValueFrom(exItem$);
-        //     focusCtx.setFocus(
-        //       EditorFocusFactory.NewActions({
-        //         exItem: exItem === false ? null : exItem,
-        //       })
-        //     );
-        //   });
-        // DexRuntime.runPromise(
-        //   effect
-        // );
-      });
+    // keyboardCtx
+    //   .onKeydown$(
+    //     "n",
+    //     viewCtx.activeWindowEqualTo$(DexWindow.ProjectEditor).pipe(
+    //       combineLatestWith(focusCtx.mapFocus$(isType(FocusFactory.None))),
+    //       map(([activeWindow, noneFocus]) => {
+    //         return activeWindow && noneFocus;
+    //       })
+    //     )
+    //   )
+    //   .subscribe(async () => {
+    //     // const effect = Effect.gen(function* () {
+    //     //     const exItem$ = yield* ExItemFocus.exItemBasicFocus$;
+    //     //     const exItem = yield* EffectUtils.firstValueFrom(exItem$);
+    //     //     focusCtx.setFocus(
+    //     //       EditorFocusFactory.NewActions({
+    //     //         exItem: exItem === false ? null : exItem,
+    //     //       })
+    //     //     );
+    //     //   });
+    //     // DexRuntime.runPromise(
+    //     //   effect
+    //     // );
+    //   });
 
     const focusIsEditorNewActions$ = focusCtx.mapFocus$((focus) => {
       return isType(focus, EditorFocusFactory.NewActions) ? focus : false;
@@ -70,28 +60,28 @@ export const EditorFocus = {
       visible$: focusIsEditorNewActions$.pipe(map((focus) => focus !== false)),
     });
 
-    keyboardCtx.onKeydown$("p", focusIsEditorNewActions$).subscribe(() => {
-      DexRuntime.runPromise(
-        Effect.gen(function* () {
-          const library = yield* LibraryCtx.library;
-          const libraryProject = yield* LibraryProjectFactory2({});
-          yield* library.libraryProjects.push(libraryProject);
-          focusCtx.popFocus();
-        })
-      );
-    });
+    // keyboardCtx.onKeydown$("p", focusIsEditorNewActions$).subscribe(() => {
+    //   DexRuntime.runPromise(
+    //     Effect.gen(function* () {
+    //       const library = yield* LibraryCtx.library;
+    //       const libraryProject = yield* LibraryProjectFactory2({});
+    //       yield* library.libraryProjects.push(libraryProject);
+    //       focusCtx.popFocus();
+    //     })
+    //   );
+    // });
 
-    keyboardCtx.onKeydown$("o", focusIsEditorNewActions$).subscribe(() => {
-      DexRuntime.runPromise(
-        Effect.gen(function* () {
-          const project = yield* Project.activeProject;
-          const exObject = yield* ExObjectFactory2({});
-          yield* project.rootExObjects.push(exObject);
-          focusCtx.popFocus();
-        })
-      );
-    });
+    // keyboardCtx.onKeydown$("o", focusIsEditorNewActions$).subscribe(() => {
+    //   DexRuntime.runPromise(
+    //     Effect.gen(function* () {
+    //       const project = yield* Project.activeProject;
+    //       const exObject = yield* ExObjectFactory2({});
+    //       yield* project.rootExObjects.push(exObject);
+    //       focusCtx.popFocus();
+    //     })
+    //   );
+    // });
 
-    keyboardCtx.registerCancel(focusIsEditorNewActions$);
+    // keyboardCtx.registerCancel(focusIsEditorNewActions$);
   }),
 };
