8938fa622fb7384f2e20e4342dc02f333ad99237
Dylan Phan <dylan.phan@tixr.com>
2024-10-22 15:48:22 -0700

    wip redo checkpoint at 10/22/24 3:48P

--- src/AppStateCtx.ts
+++ src/AppStateCtx.ts
@@ -1,6 +1,6 @@
 import assert from "assert-ts";
 import { Effect, Layer, Option, Ref, Stream, SubscriptionRef } from "effect";
-import { produce } from "immer";
+import { create } from "mutative";
 import { writable } from "svelte/store";
 import { makeAppState, ProjectEditorHome, type AppState } from "./AppState";
 import { type DexProject } from "./DexDomain";
@@ -35,7 +35,7 @@ const ctxEffect = Effect.gen(function* () {
     applyProjectReducer(fn: DexReducer.DexReducer<DexProject>) {
       return Effect.gen(function* () {
         const state = yield* appState.get;
-        const nextState = produce(state, (draft) => {
+        const nextState = create(state, (draft) => {
           const activeWindow = draft.activeWindow;
           assert(activeWindow._tag === "ProjectEditorHome", "Not in project editor");
           const dexProjectId = draft.activeProjectId;
@@ -43,14 +43,14 @@ const ctxEffect = Effect.gen(function* () {
           const project = draft.projects.find((p) => p.id === dexProjectId.value);
           assert(project !== undefined, "Project not found");
           fn(project);
-        });
+        }, { mark: () => "immutable" });
         yield* Ref.set(appState, nextState);
       });
     },
 
     applyAppStateReducer(fn: DexReducer.DexReducer<AppState>) {
       return Effect.gen(function* () {
-        const nextState = produce(yield* appState.get, fn);
+        const nextState = create(yield* appState.get, fn, { mark: () => "immutable" });
         yield* Ref.set(appState, nextState);
       });
     },
--- src/DexObject.test.ts
+++ src/DexObject.test.ts
@@ -1,5 +1,5 @@
 // sum.test.js
-import { produce } from "immer";
+import { create } from "mutative";
 import { expect, test } from "vitest";
 import { makeDexProject } from "./DexDomain";
 import { DexReducer } from "./DexReducer";
@@ -7,22 +7,25 @@ import { DexReducer } from "./DexReducer";
 test("DexObject", () => {
   const project = makeDexProject({});
   expect(project.name).toBe("Untitled Project");
-  
-  const project2 = produce(project, DexReducer.DexProject.setName("My Project"));
+
+  const project2 = create(project, DexReducer.DexProject.setName("My Project"), {
+    mark: () => "immutable",
+  });
   expect(project.name).toBe("Untitled Project");
   expect(project2.name).toBe("My Project");
-  
-  const project3 = produce(project2, DexReducer.DexProject.addComponent());
+
+  const project3 = create(project2, DexReducer.DexProject.addComponent(), { mark: () => "immutable" });
   expect(project3.components.length).toBe(1);
 
-  const project4 = produce(project3, DexReducer.DexProject.addFunction());
+  const project4 = create(project3, DexReducer.DexProject.addFunction(), { mark: () => "immutable" });
   expect(project4.functions.length).toBe(1);
 
-  const project5 = produce(project4, DexReducer.DexProject.addObject());
+  const project5 = create(project4, DexReducer.DexProject.addObject(), { mark: () => "immutable" });
   expect(project5.objects.length).toBe(1);
-  
+
   const component6 = project5.components[0]!;
-  const project6 = produce(project5, DexReducer.DexCustomComponent.setName(component6, "My Component"));
+  const project6 = create(project5, DexReducer.DexCustomComponent.setName(component6, "My Component"), {
+    mark: () => "immutable",
+  });
   expect(project6.components[0]!.name).toBe("My Component");
-  
 });
